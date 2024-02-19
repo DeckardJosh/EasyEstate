933899b7a1f9198d5c44258a1d4479d91aab649d
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-02-19 14:26:37 -0800

    Hero Completed

--- easy-estate/resources/views/livewire/home-hero.blade.php
+++ easy-estate/resources/views/livewire/home-hero.blade.php
@@ -1,11 +1,17 @@
+<?php
+    //probbaly going to need to grab value from search box for routing and query
+?>
+
 <div class="hero_wrapper">
-    <div class="hero_overlay"></div>
     <div class="hero_content">
         <h1 class="hero_header">Discover a place you'll love to live</h1>
         <div class="hero_buttons">
-            <input type="button" />
-            <input type="button" />
+            <a href="#" type="button" class="btn-secondary btn_active">Buy</a>
+            <a href="#" type="button" class="btn-secondary">Rent</a>
         </div>
-        <input type="text"  class="hero_search"/>
+        <div class="input-group mt-3">
+            <input type="text" class="form-control" placeholder="Seattle, WA">
+            <a href="#" type="button" class="input-group-text btn btn-danger" id="hero_search">Search</a>
+          </div>
     </div>
 </div>
