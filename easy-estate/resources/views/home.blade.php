29c25bfe281c7ce382c7461a5f9cf2a8dc3bfde3
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-02-18 18:11:46 -0800

    hero init and background image overlay issue

--- easy-estate/resources/views/home.blade.php
+++ easy-estate/resources/views/home.blade.php
@@ -6,9 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/hero.css') }}" />
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     @livewireStyles
-    <title>Home Page</title>
+    <title>Easy Estate Home</title>
 </head>
 <body>
     <header>
@@ -16,9 +18,12 @@
         <livewire:navbar/>
         {{-- End Navbar Livewire Component --}}
     </header>
-    <div class="hero_wrapper">
-        <p>from the body</p>
-    </div>
+    <article>
+        {{-- Hero Livewire Component --}}
+        <livewire:home-hero/>
+        {{-- End Hero Livewire Component --}}
+    </article>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     @livewireScripts
 </body>
 </html>
---
+++ easy-estate/resources/views/livewire/home-hero.blade.php
@@ -0,0 +1,11 @@
+<div class="hero_wrapper">
+    <div class="hero_overlay"></div>
+    <div class="hero_content">
+        <h1 class="hero_header">Discover a place you'll love to live</h1>
+        <div class="hero_buttons">
+            <input type="button" />
+            <input type="button" />
+        </div>
+        <input type="text"  class="hero_search"/>
+    </div>
+</div>
