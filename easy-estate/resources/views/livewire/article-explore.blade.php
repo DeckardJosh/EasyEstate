b899e119f6e9a594b84dbaf6717c7fab1f01316b
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-02-19 16:35:42 -0800

    break

--- easy-estate/resources/views/livewire/article-explore.blade.php
+++ easy-estate/resources/views/livewire/article-explore.blade.php
@@ -1,3 +1,55 @@
-<div>
-    <p>test</p>
+<div class="cust_container">
+    <h1 class="explore_article_header">Explore Homes on Easy Estate</h1>
+    <div class="explore_header_content">
+        <p class="explore_article_body">Explore Homes in the Washington Area, as well as read reviews from our previous clients.</p>
+        <span class="explore_article_disclaimer">Disclaimer: All homes are built with Mock Data, and are not actually for sale.</span>
+    </div>
+    <div class="explore_cards">
+        <div class="card text-bg-dark card_container card_single">
+
+                <div class="card-img-overlay">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                </div>
+        </div>
+        <div class="card_double">
+            <div class="card">
+                test
+            </div>
+            <div class="card">
+                test
+            </div>
+        </div>
+        <div class="card_double">
+            <div class="card">
+                test
+            </div>
+            <div class="card">
+                test
+            </div>
+        </div>
+        <div class="card_single">
+            test
+        </div>
+        <div class="card_double">
+            <div class="card">
+                test
+            </div>
+            <div class="card">
+                test
+            </div>
+        </div>
+        <div class="card_double">
+            <div class="card">
+                test
+            </div>
+            <div class="card">
+                test
+            </div>
+        </div>
+    </div>
+
+
+
 </div>
