5de9368c7004591b10729c9928f146fad7c1db95
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-02-19 18:48:07 -0800

    article complete

--- easy-estate/resources/views/livewire/article-explore.blade.php
+++ easy-estate/resources/views/livewire/article-explore.blade.php
@@ -1,55 +1,91 @@
 <div class="cust_container">
     <h1 class="explore_article_header">Explore Homes on Easy Estate</h1>
     <div class="explore_header_content">
-        <p class="explore_article_body">Explore Homes in the Washington Area, as well as read reviews from our previous clients.</p>
+        <p class="explore_article_body">Explore Homes in the Washington Area, as well as read reviews from our previous clients. We showcase homes for purchase and rent.</p>
         <span class="explore_article_disclaimer">Disclaimer: All homes are built with Mock Data, and are not actually for sale.</span>
     </div>
     <div class="explore_cards">
-        <div class="card text-bg-dark card_container card_single">
-
-                <div class="card-img-overlay">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small>Last updated 3 mins ago</small></p>
+        <div class="card_single">
+            <img src="/images/hero-family.jpg" class="card_image card_overlay_custom" alt="...">
+            <div class="single_content">
+                <h3 class="single_header">Seattle, WA</h3>
+                <button type="button" class="btn btn-light single_button">View Homes</button>
+            </div>
+        </div>
+        <div class="card_double">
+            <div class="card_double_item">
+                {{-- User Review Card --}}
+                <div class="double_user_content">
+                    <h5 class="double_user_header">EasyEstate User</h5>
+                    <p class="double_user_name">Frankie D.</p>
+                    <p class="double_user_review">I just moved into my new house 2 years ago and am loving it! The town is great, and the neighborhood is fantastic! Our...</p>
                 </div>
+                {{-- End User Review Card --}}
+            </div>
+            <div class="card_double_item">
+                <img src="/images/house-1.jpg" class="card_image card_overlay_custom" alt="...">
+                <div class="double_content">
+                    <h3 class="double_header">Olympia, WA</h3>
+                    <button type="button" class="btn btn-light double_button">View Homes</button>
+                </div>
+            </div>
         </div>
         <div class="card_double">
-            <div class="card">
-                test
+            <div class="card_double_item">
+                <img src="/images/house-2.jpg" class="card_image card_overlay_custom" alt="...">
+                <div class="double_content">
+                    <h3 class="double_header">Tacoma, WA</h3>
+                    <button type="button" class="btn btn-light double_button">View Homes</button>
+                </div>
             </div>
-            <div class="card">
-                test
-            </div>
-        </div>
-        <div class="card_double">
-            <div class="card">
-                test
-            </div>
-            <div class="card">
-                test
+            <div class="card_double_item">
+                <img src="/images/house-6.jpg" class="card_image card_overlay_custom" alt="...">
+                <div class="double_content">
+                    <h3 class="double_header">Kennewick, WA</h3>
+                    <button type="button" class="btn btn-light double_button">View Homes</button>
+                </div>
             </div>
         </div>
         <div class="card_single">
-            test
-        </div>
-        <div class="card_double">
-            <div class="card">
-                test
-            </div>
-            <div class="card">
-                test
+            <img src="/images/house-5.jpg" class="card_image card_overlay_custom" alt="...">
+            <div class="single_content">
+                <h3 class="single_header">Spokane, WA</h3>
+                <button type="button" class="btn btn-light single_button">View Homes</button>
             </div>
         </div>
-        <div class="card_double">
-            <div class="card">
-                test
+        <div class="card_double card_double_user_reveiw">
+            <div class="card_double_item">
+                <img src="/images/house-7.jpg" class="card_image card_overlay_custom" alt="...">
+                <div class="double_content">
+                    <h3 class="double_header">Vancouver, WA</h3>
+                    <button type="button" class="btn btn-light double_button">View Homes</button>
+                </div>
             </div>
-            <div class="card">
-                test
+            <div class="card_double_item">
+                {{-- User Review Card --}}
+                <div class="double_user_content_second">
+                    <h5 class="double_user_header">EasyEstate User</h5>
+                    <p class="double_user_name">Justin C.</p>
+                    <p class="double_user_review">After years of searching I finally found the perfect home. It is in a great location and the home is in perfect condition. I am so...</p>
+                </div>
+                {{-- End User Reveiw Card --}}
+            </div>
+        </div>
+        <div class="card_double">
+            <div class="card_double_item">
+                <img src="/images/house-4.jpg" class="card_image card_overlay_custom" alt="...">
+                <div class="double_content">
+                    <h3 class="double_header">Yakima, WA</h3>
+                    <button type="button" class="btn btn-light double_button">View Homes</button>
+                </div>
+            </div>
+            <div class="card_double_item">
+                <img src="/images/house-3.jpg" class="card_image card_overlay_custom" alt="...">
+                <div class="double_content">
+                    <h3 class="double_header">Kent, WA</h3>
+                    <button type="button" class="btn btn-light double_button">View Homes</button>
+                </div>
             </div>
         </div>
     </div>
-
-
-
 </div>
