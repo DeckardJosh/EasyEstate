403f5f1d51a8b3da4ee12e2e348265d9e8f08869
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-03-05 20:37:13 -0800

    footer completed

---
+++ easy-estate/resources/views/livewire/footer.blade.php
@@ -0,0 +1,13 @@
+<div class="footer_wrapper">
+    <div class="footer_copy">
+        <p class="footer_copy_one">Easy Estate is committed to ensuring digital accessibility for individuals with disabilities.  We are continuously working to improve the accessibility of our web experience for everyone,  and we welcome feedback and accommodation requests. If you wish to report an issue or seek an  accommodation, please let us know.</p>
+        <p class="footer_copy_one">Copyright © 2024 Easy Estate. All rights reserved. Have a Question? Visit our Help Center to find the answer.</p>
+        <p class="footer_copy_one">This is a mock up project intended to showcase my skills as a developer. I do not hold the rights to any of the designs, nor is this a real company. This website is not to be used as a reference for which hosue to buy. I am not liable for any miscommunication regarding this websites use.</p>
+    </div>
+    <div class="footer_image_container">
+        <image src="/images/footer_image.png" class="footer_image_one"></image>
+        <image src="/images/footer_image.png" class="footer_image"></image>
+        <image src="/images/footer_image.png" class="footer_image_one"></image>
+        <image src="/images/footer_image.png" class="footer_image"></image>
+    </div>
+</div>
