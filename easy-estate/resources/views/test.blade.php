18f77181be90a3d2c22050b3dd4038f216982a17
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-03-15 00:22:05 -0700

    website fully responsive

--- easy-estate/resources/views/test.blade.php
+++ easy-estate/resources/views/test.blade.php
@@ -6,12 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/hero.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/article-explore.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/article-house-data.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/home-help.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/about-hero.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/about-company.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/about-section.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/about-contact.css') }}" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -20,33 +19,30 @@
 </head>
 <body>
     <livewire:navbar/>
+
     <section>
-        <livewire:home-hero/>
+        {{-- about-hero comp --}}
+        <livewire:about-hero/>
+        {{-- end about-hero comp --}}
     </section>
 
     <section>
-        {{-- Explore Homes Article --}}
-        <livewire:article-explore/>
-        {{-- End Explore Homes Article --}}
+        {{-- about company comp --}}
+        <livewire:about-company/>
+        {{-- end about company comp --}}
     </section>
+
+    <article>
+        {{-- about section comp --}}
+        <livewire:about-section/>
+        {{-- end about section comp --}}
+    </article>
 
     <section>
-        {{-- Live House Data Component --}}
-        <livewire:article-house-data/>
-        {{-- End Live House Data Component --}}
+        {{-- about contact comp --}}
+        <livewire:about-contact/>
+        {{-- end contact comp --}}
     </section>
-
-    <section>
-        {{-- How can we help Component --}}
-        <livewire:home-help/>
-        {{-- End how can we help Component --}}
-    </section>
-
-    <footer>
-        {{-- Footer Component --}}
-        <livewire:footer/>
-        {{-- End Footer Component --}}
-    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="{{ asset('js/index.js') }}"></script>
