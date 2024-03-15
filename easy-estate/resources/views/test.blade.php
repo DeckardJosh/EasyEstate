28ccff0e133810da1a606005ac493598c0a41413
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-03-14 21:33:04 -0700

    article explore responsive

--- easy-estate/resources/views/test.blade.php
+++ easy-estate/resources/views/test.blade.php
@@ -24,6 +24,11 @@
         <livewire:home-hero/>
     </section>
 
+    <section>
+        {{-- Explore Homes Article --}}
+        <livewire:article-explore/>
+        {{-- End Explore Homes Article --}}
+    </section>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
