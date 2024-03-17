ff72ef89503151baab6185ea74ac01d1ebce2d6e
Josh Deckard <121363742+DeckardJosh@users.noreply.github.com>
2024-03-16 21:36:43 -0700

    1.1.1 fixed indi house responsive

--- easy-estate/resources/views/test.blade.php
+++ easy-estate/resources/views/test.blade.php
@@ -7,10 +7,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/about-hero.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/about-company.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/about-section.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/about-contact.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/house-preview.css') }}" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -21,27 +18,9 @@
     <livewire:navbar/>
 
     <section>
-        {{-- about-hero comp --}}
-        <livewire:about-hero/>
-        {{-- end about-hero comp --}}
-    </section>
-
-    <section>
-        {{-- about company comp --}}
-        <livewire:about-company/>
-        {{-- end about company comp --}}
-    </section>
-
-    <article>
-        {{-- about section comp --}}
-        <livewire:about-section/>
-        {{-- end about section comp --}}
-    </article>
-
-    <section>
-        {{-- about contact comp --}}
-        <livewire:about-contact/>
-        {{-- end contact comp --}}
+        {{-- house preview comp --}}
+        <livewire:house-preview/>
+        {{-- end house preview comp --}}
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
